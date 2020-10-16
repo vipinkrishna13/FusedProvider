832321a0f2e244f5b5daad3cde4e26361916684e
vipin <vipin@logiticks.com>
2020-10-16 22:36:29 +0530

    Initial check-in of module FusedLocationProvider

---
+++ FusedLocationProvider/FileLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace com.xamarin.samples.location.fusedlocationprovider
+{
+    public class FileLogger
+    {
+        public void LogInformation(string value)
+        {
+            try
+            {
+                string directoryPath = Android.OS.Environment.ExternalStorageDirectory.AbsolutePath;
+                var path = Path.Combine(directoryPath, "FusedLocationSampleLog");
+                string filename = Path.Combine(path, "LogFile.txt");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                var fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                // Set stream position to end-of-file
+                fs.Seek(0, SeekOrigin.End);
+
+                using (StreamWriter objStreamWriter = new StreamWriter(fs, Encoding.UTF8))
+                {
+                    objStreamWriter.WriteLine(value);
+                    objStreamWriter.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ FusedLocationProvider/FusedLocationProviderCallback.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+
+using Android.Gms.Location;
+using Android.Util;
+
+namespace com.xamarin.samples.location.fusedlocationprovider
+{
+    public class FusedLocationProviderCallback : LocationCallback
+    {
+        readonly MainActivity activity;
+        FileLogger fileLogger;
+
+        public FusedLocationProviderCallback(MainActivity activity)
+        {
+            this.activity = activity;
+            fileLogger = new FileLogger();
+        }
+
+        public override void OnLocationAvailability(LocationAvailability locationAvailability)
+        {
+            Log.Debug("FusedLocationProviderSample", "IsLocationAvailable: {0}",locationAvailability.IsLocationAvailable);
+        }
+
+
+        public override void OnLocationResult(LocationResult result)
+        {
+            if (result.Locations.Any())
+            {
+                var location = result.Locations.First();
+                activity.latitude2.Text = activity.Resources.GetString(Resource.String.latitude_string, location.Latitude);
+                activity.longitude2.Text = activity.Resources.GetString(Resource.String.longitude_string, location.Longitude);
+                activity.speed2.Text = activity.Resources.GetString(Resource.String.speed_string, location.Speed*3.6);
+                activity.provider2.Text = activity.Resources.GetString(Resource.String.requesting_updates_provider_string, location.Provider);
+                fileLogger.LogInformation($"{DateTime.Now} - Lat: {location.Latitude} , Long: {location.Longitude} , Speed: {location.Speed * 3.6}");
+            }
+            else
+            {
+                activity.latitude2.SetText(Resource.String.location_unavailable);
+                activity.longitude2.SetText(Resource.String.location_unavailable);
+                activity.speed2.SetText(Resource.String.location_unavailable);
+                activity.provider2.SetText(Resource.String.could_not_get_last_location);
+                activity.requestLocationUpdatesButton.SetText(Resource.String.request_location_button_text);
+            }
+        }
+    }
+}
